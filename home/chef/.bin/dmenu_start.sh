224c8a7530b167f29f64e289557b3e496fb4f26e
chef <chef@momofuku.my.domain>
2025-06-08 10:48:28 -0400

    adds and tweaks

--- home/chef/.bin/dmenu_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#. $HOME/.wmrc
-. $HOME/.cache/wal/colors.Xresources
-
-dmenu_run -i -p "Search" $DMENU_FN $DMENU_NB $DMENU_NF $DMENU_SF $DMENU_SB
--- home/chef/.bin/umnt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sync
-doas umount /mnt/usb 2>/dev/null
-#sudo fusermount -u /media/phone 2>/dev/null
